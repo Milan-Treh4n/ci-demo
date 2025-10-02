c4710a784269dd5a52c2ace50afb5bb162849bdd
Milan-Treh4n <trehanmilan5@gmail.com>
2025-10-02 13:23:01 +0100

    Fixed Files hopefully

---  test_example.py
+++  test_example.py
@@ -1,7 +1,7 @@
 import unittest
 from Milan_function import milan_function
 
-class TestMilanFunctions(unittest.TestCase):
+class TestMilan_Function(unittest.TestCase):
     def test_add(self):
         self.assertEqual(milan_function(6, 7), 13)
         self.assertEqual(milan_function(3.5, 2.5), 6.0)
